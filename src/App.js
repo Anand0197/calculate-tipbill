5a524e59fbae41c75864d27bfb01628291ced7bb
Anand <aanand.raj.patna@gmail.com>
2023-07-13 01:29:57 +0530

    Initial commit of this project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  return <BillCalculator />;
+}
+
+function BillCalculator() {
+  const [bill, setBill] = useState('');
+  const [per1, setPer1] = useState(0);
+  const [per2, setPer2] = useState(0);
+  const tip = (bill * (per1 + per2)) / (2 * 100);
+
+  function handleReset() {
+    setBill('');
+    setPer1(0);
+    setPer2(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calc">
+      <Bill bill={bill} setBill={setBill} />
+      <MyTip per1={per1} setPer1={setPer1} />
+      <OtherTip per2={per2} setPer2={setPer2} />
+      {bill > 0 ? <Pay bill={bill} tip={tip} onReset={handleReset} /> : ''}
     </div>
   );
 }
 
-export default App;
+function Bill({ bill, setBill }) {
+  return (
+    <div className="bill">
+      <label>How much was the bill?</label>
+      <input
+        type="text"
+        value={bill}
+        placeholder="Enter the amount of bill"
+        onChange={(e) => setBill(+e.target.value)}
+      />
+    </div>
+  );
+}
+function OtherTip({ per2, setPer2 }) {
+  return (
+    <div className="other">
+      <label>How did you Like the service?</label>
+      <select value={per2} onChange={(e) => setPer2(Number(e.target.value))}>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="5">Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+function MyTip({ per1, setPer1 }) {
+  return (
+    <div className="mytip">
+      <label>How did your friend Like the service?</label>
+      <select value={per1} onChange={(e) => setPer1(Number(e.target.value))}>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="5">Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+function Pay({ bill, tip, onReset }) {
+  return (
+    <div className="pay">
+      <h2>
+        You pay ${bill + tip} (${bill} + ${tip} tip)
+      </h2>
+      <button onClick={onReset}>Reset</button>
+    </div>
+  );
+}
